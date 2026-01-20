3ce421f6bee9db3a0bf623cb7c8e6125e045ec9a
naved255 <navedahmad2314lko@gamil.com>
2026-01-20 21:48:58 +0530

    jffj

--- Backend/session.js
+++ Backend/session.js
@@ -1,20 +1,22 @@
 import session from "express-session";
-import MongoStore from 'connect-mongo';
+import MongoStore from "connect-mongo";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default session({
-    secret: `${process.env.superSecrete}`,
-    resave:false,
-    saveUninitialized: false,
+    name: "vc-session",
+    secret: process.env.superSecrete,
+    resave: false,
+    saveUninitialized: false, // do not save empty sessions
     store: MongoStore.create({
-        mongoUrl: `${process.env.mongoUrl}`,
-        collectionName: 'sessions',
-        ttl: 14 *24 *60 *60
+        mongoUrl: process.env.mongoUrl,
+        collectionName: "sessions",
+        ttl: 14 * 24 * 60 * 60, // 14 days
     }),
     cookie: {
-        httpOnly:true,
-        secure:true,
-        sameSite:'none',
-        maxAge: 14 *24 *60 *60*1000
-
-    }
-})
+        httpOnly: true,
+        secure: true, // only true in production
+        sameSite: "none",
+        maxAge: 14 * 24 * 60 * 60 * 1000,
+    },
+});
